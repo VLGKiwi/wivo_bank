4a3da3e97fe1814fc241eb9fba1506f54a7a1ee3
VLGKiwi <zvonarev.dev@gmail.com>
2025-09-02 23:52:33 +0300

    feat: add-fonts-and-base-style-files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import localFont from 'next/font/local'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import "@styles/normalize.scss";
+import "@styles/global.scss";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = localFont({
+  src: [
+    {
+      path: "./fonts/Inter_Medium.woff2",
+      weight: "500",
+      style: "normal"
+    },
+    {
+      path: "./fonts/Inter_Regular.woff2",
+      weight: "400",
+      style: "normal"
+    }
+  ]
+})
 
 export default function RootLayout({
   children,
@@ -18,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.className}`}>
         {children}
       </body>
     </html>
